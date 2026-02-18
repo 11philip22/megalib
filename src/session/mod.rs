8904d5225cace6daabfec05ac998d3d0d3479435
11philip22 <philipwoldhek@gmail.com>
2026-02-18 10:06:12 +0100

    rename session:keys to key_sync

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -3,14 +3,14 @@
 pub mod actor;
 mod auth;
 mod device_id;
-pub mod keys;
+pub mod key_sync;
 pub mod registration;
 mod sharing;
 mod core;
 mod action_packets;
 
 pub use actor::{AccountInfo, SessionHandle};
-pub use keys::ContactPublicKeys;
+pub use key_sync::ContactPublicKeys;
 pub use registration::{RegistrationState, register, verify_registration};
 pub(crate) use core::Session;
 pub use core::{FolderSessionBlob, SessionBlob};
